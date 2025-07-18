eda8de858b405767ff4ba3ef9a1c67dadcdb8554
onco-eunho <eunhomoon@oncosoft.io>
2025-07-18 18:03:57 +0900

    [fix] fix typo

--- main.py
+++ main.py
@@ -37,14 +37,14 @@ def calculate_end_time(required_time_str, accumulated_time_str, current_time_str
         return end_time, rest_time
 
     except ValueError:
-        return "잘못된 시간 형식입니다. HH:MM:SS 형식으로 입력해 주세요."
+        return "잘못된 시간 형식입니다. HH:MM:SS 형식으로 입력해 주세요.", None
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="금요일의 퇴근 시간을 계산합니다. 필요 시간, 누적 시간, 출근 시간을 입력하세요.")
     parser.add_argument("-r", "--required", dest="required_time", required=False, help="총 필요 시간입니다. HH:MM:SS 형식으로 입력하세요. (e.g., '08:00:00').", default=config.get('app', 'default_required_time', fallback='40:00:00'))
     parser.add_argument("-a", "--accumulated", dest="accumulated_time", required=True, help="누적 시간입니다. HH:MM:SS 형식으로 입력하세요. (e.g., '01:30:00').")
     parser.add_argument("-c", "--current", dest="current_time", required=True, help="출근 시간입니다. HH:MM:SS 형식으로 입력하세요. (e.g., '14:00:00').")
-
+    
     args = parser.parse_args()
 
     end_time_result, rest_time_result = calculate_end_time(args.required_time, args.accumulated_time, args.current_time)
